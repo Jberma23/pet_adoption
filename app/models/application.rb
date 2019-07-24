5b7f3c83cfffe330e4a6792f57afbb2af3879dcf
github username <github email address>
2019-07-24 13:06:20 -0400

    stopping for lunch

--- app/models/application.rb
+++ app/models/application.rb
@@ -1,4 +1,20 @@
 class Application < ApplicationRecord
     belongs_to :pet
     belongs_to :applicant
+    accepts_nested_attributes_for :applicants
+
+    def app_id
+        self.id
+    end
+    def app_applicant
+        "#{self.applicant.first_name}  #{self.applicant.last_name} #{self.applicant.user_name}"
+    end
+    def pet_name
+        self.pet.name
+    end
+
+    def applicant_attributes=(applicant_params)
+    
+    end
+    
 end
